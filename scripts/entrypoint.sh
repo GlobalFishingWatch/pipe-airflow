563ee337368830751041d67e2e3c83c352be7445
Andrés Arana <and2arana@gmail.com>
2018-02-08 11:37:54 -0300

    Adds more logging to the wrapper script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 TRY_LOOP=10
 
+echo "Running airflow $1"
+
 # wait for DB
 if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] ; then
   echo "Waiting until database is ready at ${MYSQL_HOST}:${MYSQL_PORT}"
   i=0
-  while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
+  while ! nc -v -w 5 $MYSQL_HOST $MYSQL_PORT < /dev/null; do
     i=`expr $i + 1`
     if [ $i -ge $TRY_LOOP ]; then
       echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
